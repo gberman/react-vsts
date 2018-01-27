ba5658027607a3ebb75bcc81765d125def249950
Gregory Berman <gregory.berman@gmail.com>
2018-01-27 01:12:37 -0500

    removed unused comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import DropPlan from './DropPlan';
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,16 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <DropPlan />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="grid-container">
-          <DropPlan />
-        </div> */}
       </div>
     );
   }
